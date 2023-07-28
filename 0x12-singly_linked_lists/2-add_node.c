a379df506352003a80dfcdd860da5da84209f5aa
achrafans <achrafans3434@gmail.com>
2023-07-28 05:25:11 -0700

    23

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,8 +1,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @hd: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
 
-list_t *add_node(list_t **head, const char *str)
+
+list_t *add_node(list_t **hd, const char *str)
 {
 	list_t *new;
 	unsigned int length = 0;
@@ -16,9 +24,9 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 	new->length = length;
-	new->next = (*head);
-	(*head) = new;
+	new->next = (*hd);
+	(*hd) = new;
 
-	return (*head);
+	return (*hd);
 }
 
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of a linked list
+ * @hd: double pointer to the list_t list
+ * @str: string to put in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node_end(list_t **hd, const char *str)
 {
 	list_t *new;
-	list_t *temp = *head;
+	list_t *temp = *hd;
 	unsigned int length = 0;
 
 	while (str[length])
@@ -19,9 +26,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->length = length;
 	new->next = NULL;
 
-	if (*head == NULL)
+	if (*hd == NULL)
 	{
-		*head = new;
+		*hd = new;
 		return (new);
 	}
 
