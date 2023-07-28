7b5c7c047b496ddf47d1c5a6c49a158202460e16
achrafans <achrafans3434@gmail.com>
2023-07-28 05:18:13 -0700

    1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,12 @@
-
 #include <stdlib.h>
 #include "lists.h"
+/**
+ * list_len - returns the number of elements in a linked list
+ * @b: pointer to the list_t list
+ *
+ * Return: number of elements in b
+ */
+
 
 size_t list_len(const list_t *b)
 {
