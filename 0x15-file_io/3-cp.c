66bd22cb971e420817faa4702e116ef38df708c6
Achrafans <achrafans3434@gmail.com>
2023-08-07 11:48:22 +0100

    file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char *create_buffer(char *file);
-void close_file(int fd);
+void close_file(int fgd);
 
 /**
  * create_buffer --> Allocates 1024 bytes for a buffer.
@@ -13,43 +13,43 @@ void close_file(int fd);
  */
 char *create_buffer(char *file)
 {
-	char *buffer01;
+	char *buffer;
 
-	buffer01 = malloc(sizeof(char) * 1024);
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (buffer01 == NULL)
+	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
-	return (buffer01);
+	return (buffer);
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * close_file --> Closes file descriptors.
+ * @fgd: The file descriptor to be closed.
  */
-void close_file(int fd)
+void close_file(int fgd)
 {
 	int c;
 
-	c = close(fd);
+	c = close(fgd);
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fgd %d\n", fgd);
 		exit(100);
 	}
 }
 
 /**
- * main ---> Copies the contents of a file to another file.
+ * main --> Copies the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return:-- 0 on success--.
+ * Return: 0 on success.
  *
  * Description: If the argument count is incorrect - exit code 97.
  * If file_from does not exist or cannot be read - exit code 98.
@@ -59,7 +59,7 @@ void close_file(int fd)
 int main(int argc, char *argv[])
 {
 	int from, to, r, w;
-	char *buffer01;
+	char *buffer;
 
 	if (argc != 3)
 	{
@@ -67,9 +67,9 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	buffer01 = create_buffer(argv[2]);
+	buffer = create_buffer(argv[2]);
 	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer01, 1024);
+	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
@@ -77,7 +77,7 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer01);
+			free(buffer);
 			exit(98);
 		}
 
@@ -86,16 +86,16 @@ int main(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
-			free(buffer01);
+			free(buffer);
 			exit(99);
 		}
 
-		r = read(from, buffer01, 1024);
+		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
-	free(buffer01);
+	free(buffer);
 	close_file(from);
 	close_file(to);
 
